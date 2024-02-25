fc8a5b639da78ffc6580bc9d51c805acd34f7d9a
ilias-vs <102998139+ilias-vs@users.noreply.github.com>
2024-02-25 21:58:35 +0200

    Update LoginValidation.java

--- src/Validations/LoginValidation.java
+++ src/Validations/LoginValidation.java
@@ -10,16 +10,23 @@ import javax.swing.JOptionPane;
 import MainFrame.HomePage;
 
 public class LoginValidation {
-
+//Extend your connection class for hadling connection 
+//TODO create an Interface to hadle validation then implement ValidationClass
 
 	public LoginValidation(String username,String password) {
 		try {
+		//Call Validation interface
+		// Repository repo= new Repository();
+		//example Validation() validation= new ValidationImpl(repo.getAllUsers());
+		//Connection con = getConn();
     		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/officed1","root","MAVERIK27");
+		//Boolean flag=validation.validateUser(username,password);
     		String sql = "Select * from users where username=? and password=?";
     		PreparedStatement pst = con.prepareStatement(sql);
     		pst.setString(1, username);
     		pst.setString(2, password);
     		ResultSet rs =pst.executeQuery();
+		//if(flag).....else......
     		if(rs.next()) {
     			JOptionPane.showMessageDialog(null, "Username and Password Matched");
     			new HomePage(username,password);
